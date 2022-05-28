6a4bc12d04703390a82911b87077f830b6d98892
kissenger <ivyterrace@hotmail.co.uk>
2022-05-28 19:36:32 +0100

    first commit

---
+++ nodejs/src/app.js
@@ -0,0 +1,58 @@
+
+"use strict"
+
+/**
+ * Handles the public interface with the front-end.  Only routes are specified in this module
+ * (with some others in app-auth.js) with suppporting functions abstracted to 'app-functions.js'
+ */
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Data = require('./schema/data-model').Data;
+const dotenv = require('dotenv').config();
+if (dotenv.error) {
+  console.log(`ERROR from app.js: ${dotenv.error}`);
+  process.exit(0);
+}
+
+const app = express();
+
+// apply middleware - note setheaders must come first
+// TODO:  (2) to inject /api on routes
+app.use( (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader("Access-Control-Allow-Headers","Origin, X-Request-With, Content-Type, Accept, Authorization, Content-Disposition");
+  res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, OPTIONS");
+  next();
+});
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  // debugMsg(`${req.method}: ${req.originalUrl}`);
+  next();
+})
+
+
+// mongo as a service
+// console.log(process.env.MONGODB_PASSWORD)
+mongoose.connect(`mongodb+srv://root:${process.env.MONGODB_PASSWORD}@cluster0-5h6di.gcp.mongodb.net/iot?retryWrites=true&w=majority`,
+  {useUnifiedTopology: true, useNewUrlParser: true });
+
+mongoose.connection
+  .on('error', console.error.bind(console, 'connection error:'))
+  .on('close', () => console.log('MongoDB disconnected'))
+  .once('open', () => console.log('MongoDB connected') );
+
+
+/*****************************************************************
+ * import a route from a gpx file
+ ******************************************************************/
+app.post('/api/new-data/', async (req, res) => {
+
+//  const data = req.body.data;
+  console.log(req.body);
+  await Data.create( req.body );
+});
+
+module.exports = app;
+
---
+++ nodejs/src/schema/data-model.js
@@ -0,0 +1,22 @@
+
+const mongoose = require('mongoose');
+const dataSchema = mongoose.Schema({
+
+  sensor_name: {type: String, required: true},
+  sensor_type: {type: String, required: true},
+  time: {
+    type: Date,
+    set: d => new Date(d * 1000),
+    required: true
+  },
+  rh: {type: Number},
+  temp: {type: Number}
+
+})
+
+
+const Data = mongoose.model('data', dataSchema);
+
+module.exports = {
+  Data
+}
