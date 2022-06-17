1890f54f7095721dcc5eee50cf148b90483241b5
kissenger <ivyterrace@hotmail.co.uk>
2022-06-17 14:31:33 +0100

    update sensor_found to string var

--- nodejs/src/schema/data-model.js
+++ nodejs/src/schema/data-model.js
@@ -1,10 +1,9 @@
-
 const mongoose = require('mongoose');
 const dataSchema = mongoose.Schema({
 
   sensor_name: {type: String, required: true},
   sensor_type: {type: String, required: true},
-  sensor_found: {type: Boolean, required: true},
+  sensor_found: {type: String, required: true},
   time: {
     type: Date,
     set: d => new Date(d * 1000),
--- readme.txt
+++ readme.txt
@@ -6,3 +6,15 @@ $ sudo npm install forever -g
 run -
 $ cd nodejs
 $ forever start server.js
+
+
+Deployment
+----------
+forever list
+forever stop {{pid}}
+git fetch --all
+git reset --hard origin/master
+cp env/.env nodejs
+cd nodejs
+forever start server.js
+
