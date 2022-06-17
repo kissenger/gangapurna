91bd390691026f8eb16de5ea8beeb61299cc013b
kissenger <ivyterrace@hotmail.co.uk>
2022-06-17 14:09:49 +0100

    update scheme to include isFound

--- nodejs/src/schema/data-model.js
+++ nodejs/src/schema/data-model.js
@@ -4,6 +4,7 @@ const dataSchema = mongoose.Schema({
 
   sensor_name: {type: String, required: true},
   sensor_type: {type: String, required: true},
+  sensor_found: {type: Boolean, required: true},
   time: {
     type: Date,
     set: d => new Date(d * 1000),
---
+++ readme.txt
@@ -0,0 +1,8 @@
+to run node server:
+
+setup -
+$ sudo npm install forever -g
+
+run -
+$ cd nodejs
+$ forever start server.js
