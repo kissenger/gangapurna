fd579540b80e13458a454deda8e01e3e43e8bb60
kissenger <ivyterrace@hotmail.co.uk>
2022-09-29 14:46:34 +0100

    mongo schema enters date rather than incoming

--- nodejs/src/schema/data-model.js
+++ nodejs/src/schema/data-model.js
@@ -4,11 +4,12 @@ const dataSchema = mongoose.Schema({
   sensor_name: {type: String, required: true},
   sensor_type: {type: String, required: true},
   sensor_found: {type: String, required: true},
-  time: {
-    type: Date,
-    set: d => new Date(d * 1000),
-    required: true
-  },
+  time: {type: Date, default: Date.now, required: true},
+  // time: {
+  //   type: Date,
+  //   set: d => new Date(d * 1000),
+  //   required: true
+  // },
   deployed: {type: Boolean},
   rh: {type: Number},
   temp: {type: Number},
