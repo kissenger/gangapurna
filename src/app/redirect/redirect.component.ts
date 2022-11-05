5d02281eb880e96473134f037e53bbd889915a4c
kissenger <ivyterrace@hotmail.co.uk>
2022-11-05 18:33:47 +0000

    delete deploy-script, remove shttest page

--- src/app/redirect/redirect.component.ts
+++ src/app/redirect/redirect.component.ts
@@ -31,7 +31,7 @@ export class RedirectComponent implements OnInit {
       console.log(urlParams);
 
 
-      if ( !urlParams.zone || !['garage', 'house', 'outside', 'shtTest'].includes(urlParams.zone))  {
+      if ( !urlParams.zone || !['garage', 'house', 'outside'].includes(urlParams.zone))  {
         // if zone is not defined or of unexpected value, redirect to default path
         this.router.navigate([`/time-history/house/${startDate}/${endDate}`]);
       } else {
